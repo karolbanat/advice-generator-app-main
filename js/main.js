d153517ebc2f7c999302f3a0a4ce3b6be892f84e
Karol Banat <karol.banat@gmail.com>
2022-04-20 07:57:08 +0200

    Completed js

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const shuffleBtn = document.querySelector('.card__shuffle-btn');
+const adviceIdSpan = document.querySelector('.advice-id');
+const quoteBlock = document.querySelector('.card__quote');
+
+const fetchURL = 'https://api.adviceslip.com/advice';
+
+const getAdvice = () => {
+	fetch(fetchURL)
+		.then((responese) => responese.json())
+		.then((data) => {
+			const slipObject = data.slip;
+			adviceIdSpan.innerText = slipObject.id;
+			quoteBlock.innerText = slipObject.advice;
+		})
+		.catch((err) => console.log('Some kind of error'));
+};
+
+shuffleBtn.addEventListener('click', getAdvice);
